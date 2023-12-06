92631e9e048ad10a5f0771c24fb77a395ce8d996
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-12-06 01:13:39 -0300

    feature/update-python-logic

--- econoFraudas/python/econoFraudas.py
+++ econoFraudas/python/econoFraudas.py
@@ -1,39 +1,44 @@
-print('EcoFraudas')
+fraudas = []
 
-fraldas = []
+def calcular_fraudas():
+    nome_frauda = input("Digite a marca da fralda:")
+    valor_frauda = float(input("Digite o valor da fralda:"))
+    quantidade_frauda = int(input("Digite a quantidade de fraldas:"))
+    preço_cada_frauda = round((valor_frauda / quantidade_frauda), 2)
 
-def calcular_fraldas():
-    nome_fralda = input("Digite a marca da fralda: ")
-    valor_fralda = float(input("Digite o valor da fralda: "))
-    quantidade_fralda = int(input("Digite a quantidade de fraldas: "))
-    preço_cada_fralda = round(valor_fralda / quantidade_fralda, 2)
-
-    fralda = {
-        "nomeFralda": nome_fralda,
-        "valorFralda": valor_fralda,
-        "quantidadeFralda": quantidade_fralda,
-        "preçoCadaFralda": preço_cada_fralda
+    frauda = {
+        "nome_frauda": nome_frauda,
+        "valor_frauda": valor_frauda,
+        "quantidade_frauda": quantidade_frauda,
+        "preço_cada_frauda": preço_cada_frauda
     }
 
-    fraldas.append(fralda)
-    print(fraldas)
+    fraudas.append(frauda)
+    print(fraudas)
 
-quantas_marcas_de_fraldas = int(input("Quantas marcas de fraldas iremos avaliar hoje?"))
+quantas_marcas_de_fraudas = int(input("Quantas marcas de fraldas iremos avaliar hoje?"))
 
-for i in range(quantas_marcas_de_fraldas):
-    calcular_fraldas()
+for i in range(quantas_marcas_de_fraudas):
+    calcular_fraudas()
 
-def encontrar_fralda_mais_barata():
-    if len(fraldas) == 0:
+def encontrar_frauda_mais_barata():
+    if len(fraudas) == 0:
         print("A lista de fraldas está vazia.")
         return
 
-    valor_mais_barato = fraldas[0]
+    valor_mais_barato = fraudas[0]
 
-    for i in range(1, len(fraldas)):
-        if fraldas[i]["preçoCadaFralda"] < valor_mais_barato["preçoCadaFralda"]:
-            valor_mais_barato = fraldas[i]
+    for i in range(1, len(fraudas)):
+        if fraudas[i]["preço_cada_frauda"] < valor_mais_barato["preço_cada_frauda"]:
+            valor_mais_barato = fraudas[i]
 
-    print(f"A fralda mais barata é da marca {valor_mais_barato['nomeFralda']}, a unidade sai a R${valor_mais_barato['preçoCadaFralda']}")
+    print(f"A fralda mais barata é da marca {valor_mais_barato['nome_frauda']}, a unidade sai a R${valor_mais_barato['preço_cada_frauda']}")
 
-encontrar_fralda_mais_barata()
+encontrar_frauda_mais_barata()
+
+nova_adicao = int(input("Gostaria de adicionar mais alguma frauda? Se sim, quantas?"))
+
+for i in range(nova_adicao):
+    calcular_fraudas()
+
+encontrar_frauda_mais_barata()
